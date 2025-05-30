9176fc23d733c88edec769db29993410eaf0bd99
skoolsel <skoolsel@gmail.com>
2025-05-30 14:26:57 +0100

    Adjusted font inports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,27 +5,43 @@ import "./globals.css"
 
 const circularStd = localFont({
   src: [
-    { path: "/fonts/CircularStd-Book.woff2", weight: "400", style: "normal" },
     {
-      path: "/fonts/CircularStd-BookItalic.woff2",
+      path: "../../public/fonts/CircularStd-Book.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CircularStd-BookItalic.woff2",
       weight: "400",
       style: "italic",
     },
-    { path: "/fonts/CircularStd-Medium.woff2", weight: "500", style: "normal" },
     {
-      path: "/fonts/CircularStd-MediumItalic.woff2",
+      path: "../../public/fonts/CircularStd-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CircularStd-MediumItalic.woff2",
       weight: "500",
       style: "italic",
     },
-    { path: "/fonts/CircularStd-Bold.woff2", weight: "700", style: "normal" },
     {
-      path: "/fonts/CircularStd-BoldItalic.woff2",
+      path: "../../public/fonts/CircularStd-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CircularStd-BoldItalic.woff2",
       weight: "700",
       style: "italic",
     },
-    { path: "/fonts/CircularStd-Black.woff2", weight: "900", style: "normal" },
     {
-      path: "/fonts/CircularStd-BlackItalic.woff2",
+      path: "../../public/fonts/CircularStd-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CircularStd-BlackItalic.woff2",
       weight: "900",
       style: "italic",
     },
