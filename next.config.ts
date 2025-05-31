199e7ca66f8a4ead8f49eb18b835060ce0df5b1c
skoolsel <skoolsel@gmail.com>
2025-06-01 00:08:38 +0100

    test husky

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+// next.config.js
+import { withSentryConfig } from "@sentry/nextjs"
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  turbopack: {
+    rules: {},
+  },
+  // …any other Next.js options…
+}
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: true,
+  // silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  disableLogger: true,
+  automaticVercelMonitors: true,
+})
